64405d00c2d6dc39534a2a015ecbcb1ede95d9c8
Anatoly <d3xbrain@gmail.com>
2020-06-20 15:13:51 +0900

    create API token controller, configuration route to mobile app

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth:api')->get('/app-home', function (Request $request) {
+    return $request->user();
+});
